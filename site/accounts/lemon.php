d512fc4db1f32e430e88b4a5e45915d34ba54e72
Lemon <AhoyLemon@users.noreply.github.com>
2018-06-20 14:31:59 -0500

    new account.

---
+++ site/accounts/lemon.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: lemon
+firstname: Lemon
+lastname: 🍋
+email: lemon@thefpl.us
+password: >
+  $2a$10$POd5FFA/7eSqzH0a8YlXXeqEEr1o6ZXH6dk9cxWhiKBTnpjygsvnC
+language: en
+role: admin
